251dcba975d629eb9476e1630bc0d9b96101809f
Keerthivasan2004 <118243376+Keerthivasan2004@users.noreply.github.com>
2023-12-02 21:52:09 +0530

    Add files via upload

---
+++ leetcode/Finding First and Last Element in an Sorted Array.java
@@ -0,0 +1,73 @@
+/*Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+
+Constraints:
+
+0 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+nums is a non-decreasing array.
+-10^9 <= target <= 10^9*/
+
+Solution:
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[2];
+        res[0] = findStartPos(nums, target);
+        res[1] = findEndPos(nums, target);
+        return res;
+    }
+
+    private int findStartPos(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, ans = -1;
+        while (left <= right) {
+            int midPos = left + (right - left) / 2;
+
+            if (target == nums[midPos]) {
+                ans = midPos;
+                right = midPos - 1;
+            } else if (target > nums[midPos]) {
+                left = midPos + 1;
+            } else {
+                right = midPos - 1;
+            }
+        }
+        return ans;
+    }
+
+    private int findEndPos(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, ans = -1;
+        while (left <= right) {
+            int midPos = left + (right - left) / 2;
+
+            if (target == nums[midPos]) {
+                ans = midPos;
+                left = midPos + 1;
+            } else if (target > nums[midPos]) {
+                left = midPos + 1;
+            } else {
+                right = midPos - 1;
+            }
+        }
+        return ans;
+    }
+}
